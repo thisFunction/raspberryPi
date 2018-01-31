373228b6e7ced94e1ee5223cefa8eb6b90e21ff8
Adam Skoczylas <skocadam@gmail.com>
2018-01-31 21:48:19 +0000

    add lcd timer

---
+++ lcd-screen/printToLCD/index.js
@@ -0,0 +1,22 @@
+const Lcd = require('lcd');
+const lcd = new Lcd({
+	rs: 12,
+	e: 21,
+	data: [15, 16, 27, 28],
+     	cols: 8,
+	rows: 2
+});
+
+lcd.on('ready', function() {
+console.log('lcd ready')
+	setInterval(function() {
+		lcd.setCursor(0, 0);
+ 		lcd.print(new Date().toString().substring(16, 24));
+	}, 1000);
+});
+
+process.on('SIGINT', function() {
+	lcd.clear();
+	lcd.close();
+	process.exit();
+});
