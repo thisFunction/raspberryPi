14185c870f856706c9b3b6ee6d086236b86cd2b2
Adam Skoczylas <skocadam@gmail.com>
2018-02-01 20:10:58 +0000

    fix lcd time sript

--- lcd-screen/printToLCD/index.js
+++ lcd-screen/printToLCD/index.js
@@ -2,17 +2,21 @@ const Lcd = require('lcd');
 const lcd = new Lcd({
 	rs: 12,
 	e: 21,
-	data: [15, 16, 27, 28],
+	data: [5, 6, 17, 18],
      	cols: 8,
 	rows: 2
 });
 
 lcd.on('ready', function() {
-console.log('lcd ready')
+	console.log('lcd ready')
+
 	setInterval(function() {
 		lcd.setCursor(0, 0);
  		lcd.print(new Date().toString().substring(16, 24));
 	}, 1000);
+
+	lcd.setCursor(0,1);
+	lcd.print(`Hello world!`);
 });
 
 process.on('SIGINT', function() {
